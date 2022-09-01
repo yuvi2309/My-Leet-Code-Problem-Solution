3c62dc901635a921546522b6358045e01647fc0b
Yuvraj Nagar <88121920+yuvi2309@users.noreply.github.com>
2022-09-01 10:45:18 +0530

    Time: 10 ms (32.55%), Space: 9.6 MB (72.02%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(target==nums[i])return i;
+            else if(target < nums[i]) return i;
+        }
+        return n;
+    }
+};
