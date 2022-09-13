d8293eecf0bf7df94176746637cc9ddad576f5eb
Yuvraj Nagar <88121920+yuvi2309@users.noreply.github.com>
2022-09-13 21:30:31 +0530

    Time: 6 ms (5.84%), Space: 5.9 MB (57.08%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[n+1];
+        for(int i=0; i<=n; i++){
+            if(i==0 || i==1){
+                dp[i]=1;
+            } else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
